f0db8908b037bcaabab1fe9b0d0b7d84569a2c84
Michael Blandford <mdblandford@gmail.com>
2018-03-26 15:32:29 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -2,4 +2,4 @@ function add(a,b){return a+b}
 function subtract(a,b){return a-b}
 function multiply(a,b){return a*b}
 function divide(a,b){return a/b}
-function inc(n){return a+1}
+function inc(a){return a+1}
